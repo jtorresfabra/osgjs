f204d19907649fee03807b904b51f1eb38620f4f
Jordi Torres <jordi.torres@mirage-tech.com>
2015-06-23 08:45:22 +0200

    Changed priority

--- sources/osg/PagedLOD.js
+++ sources/osg/PagedLOD.js
@@ -221,10 +221,7 @@ define( [
                             // compute priority from where abouts in the required range the distance falls.
                             var priority = ( this._range[ numChildren ][ 0 ] - requiredRange ) / ( this._range[ numChildren ][ 1 ] - this._range[ numChildren ][ 0 ] );
                             if ( this._rangeMode === Lod.PIXEL_SIZE_ON_SCREEN ) {
-                                Matrix.transformVec3( viewModel, zeroVector, eye );
-                                var distance = Vec3.distance( eye, this.getBound().center() );
-                                priority = distance;
-                                //priority = -priority;
+                                priority = -priority;
                             }
                             // Here we do the request
                             var group = visitor.nodePath[ visitor.nodePath.length - 1 ];
