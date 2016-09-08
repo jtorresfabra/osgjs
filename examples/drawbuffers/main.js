f7616fe0ff5db0d4ea9d611e73033f71ed7b25ab
Jordi Torres <jtorresfabra@gmail.com>
2016-09-08 20:33:14 +0200

    Prototype drawBuffers

--- examples/drawbuffers/main.js
+++ examples/drawbuffers/main.js
@@ -41,9 +41,7 @@
 
             this._viewer = new osgViewer.Viewer( this._canvas );
             this._viewer.init();
-            this._viewer.getCamera().setClearColor( [ 0.0, 0.0, 0.0, 0.0 ] );
-            this._viewer.setupManipulator();
-
+            this._viewer.getCamera().setClearColor( osg.Vec4.create() );
 
             this._extDrawBuffers = this._viewer.getGraphicContext().getExtension( 'WEBGL_draw_buffers' );
             if ( !this._extDrawBuffers ) {
@@ -54,7 +52,7 @@
 
             // basic setup
             this._viewer.setSceneData( this._root );
-            this._viewer.getManipulator().computeHomePosition();
+            this._viewer.setupManipulator();
 
 
             // TODO: only run after textures and shaders loaded ?
@@ -79,6 +77,20 @@
             return depthTexture;
         },
 
+        createHUDCamera: function ( width, height ) {
+            var camera = new osg.Camera();
+            camera.setEnableFrustumCulling( true );
+            camera.setName( 'finalPass' );
+            osg.Matrix.makeOrtho( 0, width, 0, height, -5, 5, camera.getProjectionMatrix() );
+            camera.setViewMatrix( osg.Matrix.create() );
+            camera.setRenderOrder( osg.Camera.NESTED_RENDER, 0 );
+            camera.setReferenceFrame( osg.Transform.ABSOLUTE_RF );
+            camera.setViewport( new osg.Viewport( 0, 0, width, height ) );
+            camera.setClearColor( osg.Vec4.createAndSet( 0, 0, 0, 0 ) );
+            camera.setClearMask( osg.Camera.DEPTH_BUFFER_BIT );
+            return camera;
+        },
+
         createPreRenderCamera: function ( width, height ) {
             var camera = new osg.Camera();
             camera.setEnableFrustumCulling( true );
@@ -95,7 +107,7 @@
         createRTTQuad: function ( width, height ) {
             var quad = osg.createTexturedQuadGeometry( 0, 0, 0, width, 0, 0, 0, height, 0 );
             var stateSet = quad.getOrCreateStateSet();
-            stateSet.setAttributeAndModes( this.generateProgram() );
+            stateSet.setAttributeAndModes( this.generateFinalPassProgram() );
             return quad;
         },
 
@@ -106,7 +118,8 @@
                 //root.addChild( node );
                 var stateSet = node.getOrCreateStateSet();
                 stateSet.setAttributeAndModes( that.generateProgram() );
-                that._viewer.getManipulator().computeHomePosition();
+                that._viewer.getManipulator().setTarget( node.getBoundingSphere().center() );
+                that._viewer.getManipulator().setDistance( node.getBoundingSphere().radius() * 2.0 );
                 defer.resolve( node );
             } );
             return defer.promise;
@@ -150,9 +163,57 @@
             var program = new osg.Program( new osg.Shader( 'VERTEX_SHADER', vertexShader ), new osg.Shader( 'FRAGMENT_SHADER', fragmentShader ) );
             return program;
         },
+
+        generateFinalPassProgram: function () {
+            var vertexShader = [
+                '',
+                '#version 100',
+                '#ifdef GL_FRAGMENT_PRECISION_HIGH',
+                'precision highp float;',
+                '#else',
+                'precision mediump float;',
+                '#endif',
+                'attribute vec3 Vertex;',
+                'attribute vec2 TexCoord0;',
+                'varying vec2 FragTexCoord0;',
+                'uniform mat4 ModelViewMatrix;',
+                'uniform mat4 ProjectionMatrix;',
+                'void main(void) {',
+                '  gl_Position = ProjectionMatrix * ModelViewMatrix * vec4(Vertex,1.0);',
+                '  FragTexCoord0 = TexCoord0;',
+                '}',
+                ''
+            ].join( '\n' );
+            var fragmentShader = [
+                '',
+                '#ifdef GL_ES',
+                'precision highp float;',
+                '#endif',
+                '#extension GL_EXT_draw_buffers : require',
+                'varying vec2 FragTexCoord0;',
+                'uniform sampler2D Texture0;',
+                'uniform sampler2D Texture1;',
+                'uniform sampler2D Texture2;',
+                'uniform sampler2D Texture3;',
+                'uniform sampler2D DepthTexture;',
+                'void main (void)',
+                '{',
+                '  vec3 color = texture2D( Texture0, FragTexCoord0.xy).xyz;',
+                '  gl_FragData[0] = vec4( color * texture2D( DepthTexture, FragTexCoord0.xy).xyz, 1.0 );',
+                '}',
+                ''
+            ].join( '\n' );
+            var program = new osg.Program( new osg.Shader( 'VERTEX_SHADER', vertexShader ), new osg.Shader( 'FRAGMENT_SHADER', fragmentShader ) );
+            return program;
+        },
+
         createScene: function () {
             var mrtGroup = new osg.Node();
-            var camera = this.createPreRenderCamera( this._canvas.width, this._canvas.height );
+
+            var width = this._canvas.width;
+            var height = this._canvas.height;
+
+            var camera = this.createPreRenderCamera( width, height );
             this.createRTTScene().then( function ( scene ) {
                 camera.addChild( scene );
             } );
@@ -160,10 +221,10 @@
             var ext = this._extDrawBuffers;
             // createColorTextures
             for ( var i = 0; i < NUM_TEXTURES; i++ ) {
-                this._textureList[ i ] = this.createColorTexture( this._canvas.width, this._canvas.height );
+                this._textureList[ i ] = this.createColorTexture( width, height );
                 this._textureList[ i ]._name = 'texturecolor' + i;
             }
-            var depthTexture = this.createDephTexture( this._canvas.width, this._canvas.height );
+            var depthTexture = this.createDephTexture( width, height );
             this._textureList.push( depthTexture );
             // attach textures
             camera.attachTexture( ext.COLOR_ATTACHMENT0_WEBGL, this._textureList[ 0 ], 0 );
@@ -176,10 +237,27 @@
 
             this.createDebugTextureList( this._textureList, {
                 horizontal: true,
-                y: 400,
+                y: 100,
                 w: this._canvas.width * 0.2,
                 h: this._canvas.height * 0.2,
             } );
+            
+            var finalPass = new osg.Node();
+            var hudCamera = this.createHUDCamera( width, height );
+            var finalQuad = this.createRTTQuad( width, height );
+
+            hudCamera.addChild( finalQuad );
+
+            var stateSet = finalQuad.getOrCreateStateSet();
+            for ( i = 0; i < this._textureList.length; i++ ) {
+                stateSet.setTextureAttributeAndModes( i, this._textureList[ i ] );
+                stateSet.addUniform( osg.Uniform.createInt1( i, 'Texture' + i ) );
+            }
+            stateSet.setTextureAttributeAndModes( this._textureList.length, depthTexture );
+            stateSet.addUniform( osg.Uniform.createInt1( this._textureList.length, 'DepthTexture') );
+            finalPass.addChild( hudCamera );
+            mrtGroup.addChild( finalPass );
+
             return mrtGroup;
         }
     } );
